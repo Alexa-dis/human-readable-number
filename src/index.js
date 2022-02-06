6d144ccc9399673bd0a7a87d7e7a047a36eed9d8
Safonova Alexandra <alexa-09@bk.ru>
2022-02-07 00:32:38 +0300

    feat: all tests pass

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function toReadable (number) {
-  
+    if(number === 0) {
+        return 'zero';
+      }
+      let arrBeg = [' one', ' two', ' three', ' four', ' five', ' six', ' seven', ' eight', ' nine', ' ten', ' eleven', ' twelve', ' thirteen', ' fourteen', ' fifteen', ' sixteen', ' seventeen', ' eighteen', ' nineteen'];
+      let arrTen = [' twenty', ' thirty', ' forty', ' fifty', ' sixty', ' seventy', ' eighty', ' ninety'];
+      let result = [];
+
+      let ones = number % 10;
+      let tens = Math.floor(number % 100 / 10);
+      let hundred = Math.floor(number / 100);
+      
+      if(hundred != 0) {
+        result.push(arrBeg[hundred-1] + ' hundred');
+      } 
+    
+      if(tens >= 2) {
+        result.push(arrTen[tens - 2]);
+        result.push(arrBeg[ones - 1]);
+      } else {
+        result.push(arrBeg[number % 100 - 1]);
+      }
+      return result.join('').trim();
 }
